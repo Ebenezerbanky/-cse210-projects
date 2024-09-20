639c7510c427c896e7a860d1c9bd134dc6e80a65
Ebenezerbanky <bankoleebenezer111@gmail.com>
2024-09-20 23:04:54 +0100

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Step 1: Ask for the magic number
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        // Step 2: Ask the user for a guess
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        // Step 3: Compare the guess to the magic number
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
