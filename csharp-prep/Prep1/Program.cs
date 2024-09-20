44ef1ee445158daff42c54423609a5b6ad17a37e
Ebenezerbanky <bankoleebenezer111@gmail.com>
2024-09-20 02:14:12 +0100

    Added a comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        // Ask the user for their first name.
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
